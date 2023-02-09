ff28c1ffb41404c2a29376e578a1fd3b5a98dccd
Lavdim Imeri <lavdi-imeri@hotmail.com>
2023-02-09 16:37:46 +0100

    feat: DojoBlogController is added as well
    I did also tests using postman and everything works as it should!

---
+++ JavaBackend/src/main/java/com/example/demo/api/DojoBlogController.java
@@ -0,0 +1,51 @@
+package com.example.demo.api;
+
+import com.example.demo.model.DojoBlog;
+import com.example.demo.service.DojoBlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/dojoBlog")
+@RestController
+public class DojoBlogController {
+    private final DojoBlogService dojoBlogService;
+    @Autowired
+    public DojoBlogController(DojoBlogService dojoBlogService) {
+        this.dojoBlogService = dojoBlogService;
+    }
+    @PostMapping
+    public void addDojoBlog(@RequestBody DojoBlog dojoBlog){
+        dojoBlogService.addDojoBlog(dojoBlog);
+    }
+
+    @GetMapping
+    public List<DojoBlog> getAllDojoBlogs(){
+        return dojoBlogService.getAllDojoBlogs();
+    }
+
+    @GetMapping(path="{id}")
+    public DojoBlog getDojoBlogById(@PathVariable("id") UUID id){
+        return dojoBlogService.getDojoBlogById(id).orElse(null);
+    }
+
+    @DeleteMapping(path="{id}")
+    public void deleteDojoBlogById(@PathVariable("id") UUID id){
+        dojoBlogService.deleteDojoBlog(id);
+    }
+
+    @PutMapping(path="{id}")
+    public void updateDojoBlog(@PathVariable("id") UUID id, @RequestBody DojoBlog dojoBlogToUpdate){
+        dojoBlogService.updateDojoBlog(id, dojoBlogToUpdate);
+    }
+
+
+
+
+
+
+
+
+}
