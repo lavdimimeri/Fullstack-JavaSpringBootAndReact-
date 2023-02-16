d2e23220f30ad6672db892c23477169b42e08fba
Lavdim Imeri <45203738+lavdimimeri@users.noreply.github.com>
2023-02-16 16:33:22 +0100

    feat: The Script Added
    By Script I mean everytime the server runs it will create automatically 2 mock dojoblogs in order to have something to send to the client and not manually create those by using postman.

--- JavaBackend/src/main/java/com/example/demo/JavaBackendApplication.java
+++ JavaBackend/src/main/java/com/example/demo/JavaBackendApplication.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import com.example.demo.dao.DojoBlogDao;
+import com.example.demo.dao.FakeDojoBlogDataAccessService;
+import com.example.demo.model.DojoBlog;
+import com.example.demo.service.DojoBlogService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JavaBackendApplication {
 
 	public static void main(String[] args) {
+		DojoBlog dojoBlog1 = new DojoBlog(1,"firttestdojo","firsttestauthor","firsttestbody");
+		DojoBlog dojoBlog2 = new DojoBlog(2,"secondtestdojo","secondtestauthor","thirdtestbody");
+		FakeDojoBlogDataAccessService fk = new FakeDojoBlogDataAccessService();
+        fk.insertDojoBlog(dojoBlog1);
+	    fk.insertDojoBlog(dojoBlog2);
 		SpringApplication.run(JavaBackendApplication.class, args);
 	}
 
